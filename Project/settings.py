be8157707727d1ff11c3764d1b972af5f8a20d3b
ProgramadorGod <pipenet12@gmail.com>
2025-04-22 00:06:59 -0500

    domains

--- Project/settings.py
+++ Project/settings.py
@@ -100,6 +100,7 @@ CORS_ALLOWED_ORIGINS = [
     "https://atlas.thiup.com",
     "https://teststrack.thiup.com",    
     "http://teststrack.thiup.com.s3-website.us-east-2.amazonaws.com",
+    "http://teststrack.thiup.com.s3-website.us-east-2.amazonaws.com/",
     "http://192.168.0.19:3000",
     "http://teststrack.thiup.com.s3-website.us-east-2.amazonaws.com/",
     "http://teststrack.thiup.com.s3-website-us-east-1.amazonaws.com",
@@ -119,7 +120,7 @@ CSRF_TRUSTED_ORIGINS = [
 
     "http://192.168.0.19:3000",
     "http://teststrack.thiup.com.s3-website.us-east-2.amazonaws.com/",
-    "",
+    
     "https://teststrack.thiup.com/"
     "http://teststrack.thiup.com.s3-website-us-east-1.amazonaws.com/",
 
@@ -131,10 +132,10 @@ CORS_ALLOW_CREDENTIALS = True
 CORS_ORIGIN_WHITELIST = [
     "https://teststrack.thiup.com/",
     "http://teststrack.thiup.com.s3-website.us-east-2.amazonaws.com",
-
+    "http://teststrack.thiup.com.s3-website.us-east-2.amazonaws.com/",
     "http://192.168.0.19:3000",
     "https://atlas.thiup.com/",
-    
+
     "http://teststrack.thiup.com.s3-website-us-east-1.amazonaws.com/",
   # La URL de tu frontend
 ]
